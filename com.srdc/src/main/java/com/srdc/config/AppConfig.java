4a278cde3526e88d957f2bd897e2236ecee5c0ca
darshan <mailshahdarshan@gmail.com>
2015-09-12 01:11:41 +0530

    Signed-off-by: darshan <mailshahdarshan@gmail.com>

---
+++ com.srdc/src/main/java/com/srdc/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.srdc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages={"com.srdc.controller"})
+public class AppConfig {
+
+	@Bean
+	public InternalResourceViewResolver viewResolver(){
+		InternalResourceViewResolver view = new InternalResourceViewResolver();
+		view.setPrefix("/WEB-INF/views/");
+		view.setSuffix(".jsp");
+		return view;
+	}
+}
